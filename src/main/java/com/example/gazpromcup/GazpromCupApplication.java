57367d5580c1e03c641a169c133e02f315e91d4a
xeonstaf <alekseiasta@gmail.com>
2022-11-07 20:34:56 +0300

    feat: Initial commit

---
+++ src/main/java/com/example/gazpromcup/GazpromCupApplication.java
@@ -0,0 +1,13 @@
+package com.example.gazpromcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazpromCupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GazpromCupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gazpromcup/GazpromCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gazpromcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazpromCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
